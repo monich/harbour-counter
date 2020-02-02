5c15ac82dc829b4a8a794bae1496b1bb1002deef
A <hucul@o2.pl>
2020-02-02 11:39:40 +0100

    Create harbour-counter-pl.ts
    Polish translation

---
+++ translations/harbour-counter-pl.ts
@@ -0,0 +1,72 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl">
+<context>
+    <name></name>
+    <message id="counter-app_name">
+        <source>Counter</source>
+        <extracomment>Application title</extracomment>
+        <translation>Counter</translation>
+    </message>
+    <message id="counter-menu-delete_counter">
+        <source>Delete this counter</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Usuń ten licznik</translation>
+    </message>
+    <message id="counter-menu-new_counter">
+        <source>New counter</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Nowy licznik</translation>
+    </message>
+    <message id="counter-remorse-delete_counter">
+        <source>Deleting this counter</source>
+        <extracomment>Remorse popup text</extracomment>
+        <translation>Usuwanie tego licznika</translation>
+    </message>
+    <message id="counter-default_title">
+        <source>Counter</source>
+        <extracomment>Default title for a new counter</extracomment>
+        <translation>Licznik</translation>
+    </message>
+    <message id="counter-title">
+        <source>Label</source>
+        <extracomment>Text field label (counter title)</extracomment>
+        <translation>Etykieta</translation>
+    </message>
+    <message id="counter-title-placeholder">
+        <source>Short description of this counter</source>
+        <extracomment>Text field placeholder (counter title)</extracomment>
+        <translation>Krótki opis tego licznika</translation>
+    </message>
+    <message id="counter-switch-favorite">
+        <source>Show on cover</source>
+        <extracomment>Text switch label</extracomment>
+        <translation>Pokaż na etykiecie</translation>
+    </message>
+    <message id="counter-switch-favorite-description">
+        <source>No more than two counters can be shown on cover at the same time.</source>
+        <extracomment>Text switch description</extracomment>
+        <translation>Nie można jednocześnie wyświetlać więcej niż dwóch liczników na okładce.</translation>
+    </message>
+    <message id="counter-switch-sounds">
+        <source>Play sounds</source>
+        <extracomment>Text switch label</extracomment>
+        <translation>Odtwarzaj dźwięki</translation>
+    </message>
+    <message id="counter-button-reset">
+        <source>Reset</source>
+        <extracomment>Button label (resets counter to zeto)</extracomment>
+        <translation>Reset</translation>
+    </message>
+    <message id="counter-label-last_change">
+        <source>Last change:</source>
+        <extracomment>Label text (time and date of the last change)</extracomment>
+        <translation>Ostatnia zmiana:</translation>
+    </message>
+    <message id="counter-label-reset">
+        <source>Reset:</source>
+        <extracomment>Label text (time and date of the last reset)</extracomment>
+        <translation>Reset:</translation>
+    </message>
+</context>
+</TS>
