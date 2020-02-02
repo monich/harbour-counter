3368c6f83bc6d676c3dd7dceb0a4ed6925691b01
Åke Engelbrektson <eson57@users.noreply.github.com>
2020-02-02 09:42:44 +0100

    Create harbour-counter-sv.ts
    Please add Swedish translation.

---
+++ translations/harbour-counter-sv.ts
@@ -0,0 +1,72 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name></name>
+    <message id="counter-app_name">
+        <source>Counter</source>
+        <extracomment>Application title</extracomment>
+        <translation>Counter</translation>
+    </message>
+    <message id="counter-menu-delete_counter">
+        <source>Delete this counter</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Ta bort den här räknaren</translation>
+    </message>
+    <message id="counter-menu-new_counter">
+        <source>New counter</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Ny räknare</translation>
+    </message>
+    <message id="counter-remorse-delete_counter">
+        <source>Deleting this counter</source>
+        <extracomment>Remorse popup text</extracomment>
+        <translation>Tar bort denna räknare</translation>
+    </message>
+    <message id="counter-default_title">
+        <source>Counter</source>
+        <extracomment>Default title for a new counter</extracomment>
+        <translation>Räknare</translation>
+    </message>
+    <message id="counter-title">
+        <source>Label</source>
+        <extracomment>Text field label (counter title)</extracomment>
+        <translation>Etikett</translation>
+    </message>
+    <message id="counter-title-placeholder">
+        <source>Short description of this counter</source>
+        <extracomment>Text field placeholder (counter title)</extracomment>
+        <translation>Kort beskrivning av denna räknare</translation>
+    </message>
+    <message id="counter-switch-favorite">
+        <source>Show on cover</source>
+        <extracomment>Text switch label</extracomment>
+        <translation>Visa på programminiatyren</translation>
+    </message>
+    <message id="counter-switch-favorite-description">
+        <source>No more than two counters can be shown on cover at the same time.</source>
+        <extracomment>Text switch description</extracomment>
+        <translation>Inte mer än två räknare kan visas samtidigt på programminiatyren.</translation>
+    </message>
+    <message id="counter-switch-sounds">
+        <source>Play sounds</source>
+        <extracomment>Text switch label</extracomment>
+        <translation>Spela upp ljud</translation>
+    </message>
+    <message id="counter-button-reset">
+        <source>Reset</source>
+        <extracomment>Button label (resets counter to zeto)</extracomment>
+        <translation>Återställ</translation>
+    </message>
+    <message id="counter-label-last_change">
+        <source>Last change:</source>
+        <extracomment>Label text (time and date of the last change)</extracomment>
+        <translation>Senaste ändring:</translation>
+    </message>
+    <message id="counter-label-reset">
+        <source>Reset:</source>
+        <extracomment>Label text (time and date of the last reset)</extracomment>
+        <translation>Återställd:</translation>
+    </message>
+</context>
+</TS>
