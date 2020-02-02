0eb0526130f9dde895b772a0a59d178c69be611e
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2020-02-02 15:16:14 +0800

    Create harbour-counter-zh_CN.ts
    Chinese  translation

---
+++ translations/harbour-counter-zh_CN.ts
@@ -0,0 +1,72 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name></name>
+    <message id="counter-app_name">
+        <source>Counter</source>
+        <extracomment>Application title</extracomment>
+        <translation>计数器</translation>
+    </message>
+    <message id="counter-menu-delete_counter">
+        <source>Delete this counter</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>删除该计数器</translation>
+    </message>
+    <message id="counter-menu-new_counter">
+        <source>New counter</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>新计数器</translation>
+    </message>
+    <message id="counter-remorse-delete_counter">
+        <source>Deleting this counter</source>
+        <extracomment>Remorse popup text</extracomment>
+        <translation>正在删除该计数器</translation>
+    </message>
+    <message id="counter-default_title">
+        <source>Counter</source>
+        <extracomment>Default title for a new counter</extracomment>
+        <translation>计数器</translation>
+    </message>
+    <message id="counter-title">
+        <source>Label</source>
+        <extracomment>Text field label (counter title)</extracomment>
+        <translation>标签</translation>
+    </message>
+    <message id="counter-title-placeholder">
+        <source>Short description of this counter</source>
+        <extracomment>Text field placeholder (counter title)</extracomment>
+        <translation>关于该标签的简短描述</translation>
+    </message>
+    <message id="counter-switch-favorite">
+        <source>Show on cover</source>
+        <extracomment>Text switch label</extracomment>
+        <translation>显示于封面</translation>
+    </message>
+    <message id="counter-switch-favorite-description">
+        <source>No more than two counters can be shown on cover at the same time.</source>
+        <extracomment>Text switch description</extracomment>
+        <translation>封面最多只能同时显示两个计时器。</translation>
+    </message>
+    <message id="counter-switch-sounds">
+        <source>Play sounds</source>
+        <extracomment>Text switch label</extracomment>
+        <translation>显示秒数</translation>
+    </message>
+    <message id="counter-button-reset">
+        <source>Reset</source>
+        <extracomment>Button label (resets counter to zeto)</extracomment>
+        <translation>重置</translation>
+    </message>
+    <message id="counter-label-last_change">
+        <source>Last change:</source>
+        <extracomment>Label text (time and date of the last change)</extracomment>
+        <translation>最近修改：</translation>
+    </message>
+    <message id="counter-label-reset">
+        <source>Reset:</source>
+        <extracomment>Label text (time and date of the last reset)</extracomment>
+        <translation>重置：</translation>
+    </message>
+</context>
+</TS>
